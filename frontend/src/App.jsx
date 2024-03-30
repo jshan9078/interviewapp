e7e9f7a26779be8ef9722a076ace422f7fb74260
My name <me@example.com>
2024-03-30 19:10:40 -0400

    fontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [recording_state, setRecord] = useState(0)
 
   return (
     <>
-      
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit"></link>
       <div>
       <nav class="navbar">
           <img src="../src/assets/Logo.PNG" class="logo" alt=""/>
@@ -19,14 +21,14 @@ function App() {
       <div class="left-panel">
         <h2>Tell us a bit about your interview:</h2>
         <form>
-          <input type="text" placeholder="Enter your name"></input>
-          <input type="text" placeholder="Enter your interviewee's name"></input>
-          <input type="text" placeholder="What position are you interviewing for?"></input>
+          <input type="text" name="interviewer_name" placeholder="Enter your name"></input>
+          <input type="text" name="interviewee_name" placeholder="Enter your interviewee's name"></input>
+          <input type="text" name="job_position" placeholder="What position are you interviewing for?"></input>
           <label for="job-desc">Upload Job Description</label>
-          <input type="file" id="job-desc"></input>
+          <input type="file" onChange={handleFileSelected} name="file_uploaded" id="job-desc"></input>
           <div class="buttons">
-            <button type="button" id="start">Start Interview</button>
-            <button type="button" id="end">End Interview</button>
+            <button type="button" name="interview_start" id="start" onClick={() => setRecord((recording_state) => recording_state=1)}>Start Interview</button>
+            <button type="button" name="interview_stop" id="end" onClick={() => setRecord((recording_state) => recording_state=0)}>End Interview</button>
           </div>
         </form>
       </div>
